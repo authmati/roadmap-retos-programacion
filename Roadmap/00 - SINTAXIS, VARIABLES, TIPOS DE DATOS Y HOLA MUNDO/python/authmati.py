758f167f7acfc7aea6be02bfb03a1e89c4c83e91
authmati <matiasayala517@gmail.com>
2025-01-24 00:15:50 -0300

    #00-python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/authmati.py
@@ -0,0 +1,42 @@
+""" 
+- Crea un comentario en el código y coloca la URL del sitio web oficial 
+del lenguaje de programación que has seleccionado. 
+"""
+
+# Sitio oficial de Python: https://www.python.org/
+
+"""
+- Representa las diferentes sintaxis que existen de crear comentarios
+en el lenguaje (en una línea, varias...). 
+"""
+
+# Este es un comentario de una linea.
+
+"""
+Esto es un comentario de varias lineas
+en python, saludos.
+"""
+
+""" 
+Crea una variable (y una constante si el lenguaje lo soporta).
+"""
+
+variable = 4  # Esto es una variable
+CONSTANTE = 2  # Esto es una constante por convencion, como tal no hay una constante definida en python
+
+"""  
+- Crea variables representando todos los tipos de datos primitivos
+del lenguaje (cadenas de texto, enteros, booleanos...).
+"""
+
+texto = "Mati"  # Esto es un string
+enteros = 23  # Esto es un int
+decimales = 15.3  # Esto es un float
+booleano = True  # Esto es un boolean
+booleano_dos = False  # Esto es un boolean
+
+"""  
+- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+"""
+
+print("¡Hola, Python!")
