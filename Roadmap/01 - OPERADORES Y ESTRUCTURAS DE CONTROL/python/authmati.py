705cbb2d39460869b757cb28e9a95cf1d3235606
authmati <matiasayala517@gmail.com>
2025-01-25 16:19:28 -0300

    #01-python

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/authmati.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/authmati.py
@@ -4,6 +4,37 @@ Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bi
 (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
 """
 
+numero_uno = 5
+numero_dos = 4
+
+print("OPERADORES ARITMETICOS: \n")
+
+print(f"01- Suma: {numero_uno + numero_dos}") # Da la suma entre dos numeros
+print(f"02- Resta: {numero_uno - numero_dos}") # Da la resta entre dos numeros
+print(f"03- Multiplicacion: {numero_uno * numero_dos}") # Multiplicacion de dos numeros
+print(f"04- Division: {numero_uno / numero_dos}") # Divison entre dos numeros
+print(f"04- Modulo: {numero_uno % numero_dos}") # Da el resto de la division entre dos numeros
+print(f"05- Exponente: {numero_uno ** numero_dos}") # Eleva el numero de la izquierda a la potencia del numero del lado derecho
+print(f"06 - Division entera: {numero_uno // numero_dos}") # Se usa para dividir dos numeros enteros y redondear el resultado hacia abajo
+
+print("OPERADORES LOGICOS: \n")
+
+print(f"01- AND: {numero_uno == numero_dos and numero_uno == numero_dos}") # Evalua si dos expresiones booleanas son verdaderas y devuelve True si ambas lo son
+print(f"02- OR: {numero_uno == numero_dos or numero_uno == numero_dos}") #  Devuelve True si al menos uno de los valores es True
+print(f"03- NOT: {not numero_uno == numero_dos}") # Invierte el el valor de verdad de un valor
+
+print("OPERADORES DE COMPARACION: \n")
+
+print(f"01- Igualdad: {numero_uno == numero_dos}") # Compara si los valores son iguales
+print(f"02- Distinto: {numero_uno != numero_dos}") # Compara si los dos valores son distintos
+print(f"03- Mayor que: {numero_uno > numero_dos}") # Compara si el valores de lado izquierdo es mayor que el derecho
+print(f"Menor que: {numero_uno < numero_dos}") # Compara si el valores del lado derecho es mayor que el izquierdo
+print(f"Mayor o igual que: {numero_uno >= numero_dos}") # Compara si el valor del lado izquierdo es mayor o igual al derecho
+print(f"Mnoer o igual que: {numero_uno <= numero_dos}") # Compara si el valor del lado derecho es mayor o igual al izquierdo
+
+print("OPERADORES DE ASIGNACION: \n")
+
+
 """ 
 - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
 que representen todos los tipos de estructuras de control que existan
